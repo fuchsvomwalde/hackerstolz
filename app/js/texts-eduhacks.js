9edff9c916b0885f142cebe918761bc565a78146
fuchsvomwalde <jascha.quintern@googlemail.com>
2015-03-09 14:40:16 +0100

    translations eduhacks

--- app/js/texts-eduhacks.js
+++ app/js/texts-eduhacks.js
@@ -3,5 +3,137 @@ var _ = {
     about: {
         DE: "Übersicht",
         EN: "About"
+    }, partners: {
+        DE: "Partner",
+        EN: "Partners"
+    }, faq: {
+        DE: "FAQ",
+        EN: "FAQ"
+    }, sponsoring: {
+        DE: "Sponsoring",
+        EN: "Sponsoring"
+    }, intro1: {
+        DE: "...",
+        EN: "EduHacks Mannheim is the first hackathon taking place at the University of Mannheim. The event brings students, educators and technologists together to share ideas, form teams, design digital products and rapidly build innovative solutions that tackle today's educational challenges."
+    }, intro2: {
+        DE: "...",
+        EN: "Are you an organization interested in sponsoring our event to get in touch with highly motivated students?"
+    }, click: {
+        DE: "...",
+        EN: "Click here"
+    }, intro3: {
+        DE: "...",
+        EN: "Scroll down for more information. Details are coming soon. <span class='text-info'>&gt;_ Stay tuned</span>"
+    }, about_intro: {
+        DE: "...",
+        EN: "Click here"
+    }, about_p1: {
+        DE: "...",
+        EN: "This time it's all about education, so build apps that help learning, analyze big data from the education sector, build quizzes for everyone etc."
+    }, about_p2: {
+        DE: "...",
+        EN: "Meet new friends, partners or future employees."
+    }, about_p3: {
+        DE: "...",
+        EN: "Organized by the association <a href='http://www.hackerstolz.de'>Hackerstolz</a> in cooperation with the <a href='http://www.mcei.de'>Mannheim Center for Innovation and Entrepreneurship</a> and the <a href='http://blog.squareroots.de/en/'>SquareRoots Mannheim</a>"
+    }, about_p4: {
+        DE: "...",
+        EN: "When: <strong>April 2015</strong>"
+    }, about_p5: {
+        DE: "...",
+        EN: "Where: <strong>University of Mannheim</strong>"
+    }, faq1_head: {
+        DE: "...",
+        EN: "Why education as the guiding theme?"
+    }, faq1_intro: {
+        DE: "...",
+        EN: "Education is a major challenge in today's society. It is so essential because..."
+    }, faq1_p1: {
+        DE: "...",
+        EN: "there is an ever increasing amount of things to learn"
+    }, faq1_p2: {
+        DE: "...",
+        EN: "there are more opportunities to learn than ever before"
+    }, faq1_p3: {
+        DE: "...",
+        EN: "it becomes harder to decide what is worth learning"
+    }, faq2_head: {
+        DE: "...",
+        EN: "What to expect?"
+    }, faq2_intro: {
+        DE: "...",
+        EN: "During the EduHack Mannheim, teams will come together to revise what and how society learns in a creative and open-minded environment. The results will be prototypes that outline their ideas. These could be, but are not limited to projects that..."
+    }, faq2_p1: {
+        DE: "...",
+        EN: "process and visualize data to facilitate learning about a certain topic"
+    }, faq2_p2: {
+        DE: "...",
+        EN: "tools that give recommendations about what to learn"
+    }, faq2_p3: {
+        DE: "...",
+        EN: "tools that track and streamline the learning progress"
+    }, faq2_p4: {
+        DE: "...",
+        EN: "analyses about the current state of the education sector based on public data sets"
+    }, faq3_head: {
+        DE: "...",
+        EN: "Why attend?"
+    }, faq3_p1: {
+        DE: "...",
+        EN: "1. Learn new skills and apply the latest technology."
+    }, faq3_p2: {
+        DE: "...",
+        EN: "2. Build your network by collaborating with innovators and experts across multiple disciplines."
+    }, faq3_p3: {
+        DE: "...",
+        EN: "3. Build a technological prototype as a solution to real world challenges in education."
+    }, faq3_p4: {
+        DE: "...",
+        EN: "4. Join a community of people committed to igniting innovation in digital education."
+    }, faq3_p5: {
+        DE: "...",
+        EN: "5. Win awesome prices and magnify your reputation by sharing your ideas."
+    }, faq3_p6: {
+        DE: "...",
+        EN: "6. Meet new people & have lots of fun!"
+    }, faq4_head: {
+        DE: "...",
+        EN: "Who is EduHacks for?"
+    }, faq4_intro: {
+        DE: "...",
+        EN: "Everyone is welcome to join the event. We strive for an interdisciplinary mixture of participants, where everybody can contribute their specific skills. However, we recommend having a background in one of the following areas:"
+    }, faq4_p1: {
+        DE: "...",
+        EN: "Programming"
+    }, faq4_p2: {
+        DE: "...",
+        EN: "Web Design"
+    }, faq4_p3: {
+        DE: "...",
+        EN: "Statistics & Data Analysis"
+    }, faq4_p4: {
+        DE: "...",
+        EN: "Communication Design"
+    }, faq4_p4: {
+        DE: "...",
+        EN: "Educating or teaching"
+    }, sponsoring_intro: {
+        DE: "...",
+        EN: "The ideal way to get in contact with highly motivated students and find future employees"
+    }, sponsoring_p1: {
+        DE: "...",
+        EN: "Different sponsoring packages are available"
+    }, sponsoring_p2: {
+        DE: "...",
+        EN: "Participants from different fields of study: Business, IT, Design and others..."
+    }, sponsoring_p3: {
+        DE: "...",
+        EN: "Contact us for further information"
+    }, sponsoring_p4: {
+        DE: "...",
+        EN: "<a class='btn btn btn-lg' target='_blank' href='docs/sponsorenpakete-2015-v2-EN.pdf' role='button'>Available Sponsoring Packages (EN)</a><a class='btn btn btn-lg' target='_blank' href='docs/sponsorenpakete-2015-v2-DE.pdf' role='button'>Available Sponsoring Packages (DE)</a>"
+    }, sponsoring_p5: {
+        DE: "...",
+        EN: "<a class='btn btn-primary btn-lg' href='mailto:sponsoring@hackerstolz.de?subject=Sponsoring EDUHacks' role='button'>Send us an email!</a>"
     }
 };
