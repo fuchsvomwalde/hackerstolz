59c8139dfde9cd67347f9cd1b648070016b280b3
Alexander Mueller <alexan.chr.mueller@gmail.com>
2015-03-11 10:43:36 +0100

    Added German translation for eduhacks page

--- app/js/texts-eduhacks.js
+++ app/js/texts-eduhacks.js
@@ -13,127 +13,127 @@ var _ = {
         DE: "Sponsoring",
         EN: "Sponsoring"
     }, intro1: {
-        DE: "...",
+        DE: "EDUHack Mannheim ist der erste Hackathon, der an der Universität Mannheim veranstaltet wird. Die Veranstaltung soll Studenten, Technologyexperten und Personen aus dem Bildungsbereich zusammenbringen. So sollen Ideen in interdisziplinarer Zusammenarbeit entstehen und als digitale Prototypen realisiert werden. So wollen wir einen Beitrag zur Lösung der Problem im Bildungssektor leisten. ",
         EN: "EduHacks Mannheim is the first hackathon taking place at the University of Mannheim. The event brings students, educators and technologists together to share ideas, form teams, design digital products and rapidly build innovative solutions that tackle today's educational challenges."
     }, intro2: {
-        DE: "...",
+        DE: "Sind Sie als Organisation an einer Kooperation in Form von Sponsoring interessiert?",
         EN: "Are you an organization interested in sponsoring our event to get in touch with highly motivated students?"
     }, click: {
-        DE: "...",
+        DE: "Klicken Sie hier",
         EN: "Click here"
     }, intro3: {
-        DE: "...",
+        DE: "Scrolle weiter nach unten für weitere Informationen. <span class='text-info'>&gt;_ Stay tuned</span> ",
         EN: "Scroll down for more information. Details are coming soon. <span class='text-info'>&gt;_ Stay tuned</span>"
     }, about_intro: {
-        DE: "...",
-        EN: "Click here"
+        DE: "Wir wollen ein Event veranstalten, welches  Studenten und Berufseinsteiger zusammenbringt, um neue Ideen als Prototyp zu realisieren.",
+        EN: "We want to build a platform for students (from all disciplines) and young professionals to prototype awesome ideas."
     }, about_p1: {
-        DE: "...",
+        DE: "Diesesmal lautet das Thema Education (deutsch:Bildung). Dementsprechend sollen Apps programiert, Daten analysiert, Spiele u.v.m gebaut werden, die den Begriff lernen neu definieren. ",
         EN: "This time it's all about education, so build apps that help learning, analyze big data from the education sector, build quizzes for everyone etc."
     }, about_p2: {
-        DE: "...",
+        DE: "Treffe Freunde, Partner oder potentielle Angestellte",
         EN: "Meet new friends, partners or future employees."
     }, about_p3: {
-        DE: "...",
+        DE: "Organisiert von dem Verein  <a href='http://www.hackerstolz.de'>Hackerstolz</a> in Kooperation mit dem <a href='http://www.mcei.de'>Mannheim Center for Innovation and Entrepreneurship</a> und der Studenteninitiative <a href='http://blog.squareroots.de/en/'>SquareRoots Mannheim</a>",
         EN: "Organized by the association <a href='http://www.hackerstolz.de'>Hackerstolz</a> in cooperation with the <a href='http://www.mcei.de'>Mannheim Center for Innovation and Entrepreneurship</a> and the <a href='http://blog.squareroots.de/en/'>SquareRoots Mannheim</a>"
     }, about_p4: {
-        DE: "...",
+        DE: "Wann: <strong>April 2015</strong>",
         EN: "When: <strong>April 2015</strong>"
     }, about_p5: {
-        DE: "...",
+        DE: "Wo: <strong>Universität Mannheim</strong>",
         EN: "Where: <strong>University of Mannheim</strong>"
     }, faq1_head: {
-        DE: "...",
+        DE: "Warum Education als Thema?",
         EN: "Why education as the guiding theme?"
     }, faq1_intro: {
-        DE: "...",
+        DE: "Der Bildungsbereich steht vor großen Herausforderungen in der heutigen Gesellschaft, weil ..",
         EN: "Education is a major challenge in today's society. It is so essential because..."
     }, faq1_p1: {
-        DE: "...",
+        DE: "die Welt wird immer schneller und so wächst die Anzahl der Fähigkeiten, die es Wert sind zu lernen",
         EN: "there is an ever increasing amount of things to learn"
     }, faq1_p2: {
-        DE: "...",
+        DE: "es gibt eine unüberschaubare Menge von Lernangeboten (Online Kurse, Freie Tutorials, etc.) ",
         EN: "there are more opportunities to learn than ever before"
     }, faq1_p3: {
-        DE: "...",
+        DE: "es fällt schwer zu entscheiden, welche Konzepte oder Technologien es wirklich Wert sind zu lernen",
         EN: "it becomes harder to decide what is worth learning"
     }, faq2_head: {
-        DE: "...",
+        DE: "Was erwartet Euch auf dem Event?",
         EN: "What to expect?"
     }, faq2_intro: {
-        DE: "...",
+        DE: "Während des Hackathon, werden sich die Teilnehmer in Teams zusammenfinden, um zu analysieren, wie ein kreativer Lernprozess in einer offenen Zukunftsgesellschaft aussehen sollen. Diese Konzepte werden dann in digitale Prototypen übertragen, welche die Ideen als Hack verdeutlichen sollen. Einige Anregungen könnten sein",
         EN: "During the EduHack Mannheim, teams will come together to revise what and how society learns in a creative and open-minded environment. The results will be prototypes that outline their ideas. These could be, but are not limited to projects that..."
     }, faq2_p1: {
-        DE: "...",
+        DE: "Verarbeite, analysiere und visualiere Daten, um es Benutzern zu ermögliches neues über ein bestimmtes Thema zu lernen.",
         EN: "process and visualize data to facilitate learning about a certain topic"
     }, faq2_p2: {
-        DE: "...",
+        DE: "Tools, die Vorschläge für Nutzer generieren, was Sie als nächstes Lernen sollten",
         EN: "tools that give recommendations about what to learn"
     }, faq2_p3: {
-        DE: "...",
+        DE: "Self-Tracking Tools die den Lernprozess von Nutzern überwachen und verbessern",
         EN: "tools that track and streamline the learning progress"
     }, faq2_p4: {
-        DE: "...",
-        EN: "analyses about the current state of the education sector based on public data sets"
+        DE: "Analysiere Datensätze aus dem öffentlichen Sektor über Bildungsausgaben",
+        EN: "analysis about the current state of the education sector based on public data sets"
     }, faq3_head: {
-        DE: "...",
+        DE: "Warum sollte ich teilnehmen?",
         EN: "Why attend?"
     }, faq3_p1: {
-        DE: "...",
+        DE: "1. Lerne neues und probiere neuste Technologien aus",
         EN: "1. Learn new skills and apply the latest technology."
     }, faq3_p2: {
-        DE: "...",
+        DE: "2. Vergrößere dein Netzwerk in dem du mit innovativen Personen aus unterschiedlichsten Bereichen zusammenarbeitest",
         EN: "2. Build your network by collaborating with innovators and experts across multiple disciplines."
     }, faq3_p3: {
-        DE: "...",
+        DE: "3. Entwickle digitale Prototypen als Lösung für Probleme, die in der Welt zu lösen sind",
         EN: "3. Build a technological prototype as a solution to real world challenges in education."
     }, faq3_p4: {
-        DE: "...",
+        DE: "4. Werde teil einer Community, die neue innovative Ansätze der digitalen Bildung fördern will",
         EN: "4. Join a community of people committed to igniting innovation in digital education."
     }, faq3_p5: {
-        DE: "...",
+        DE: "5. Gewinne Preise und gewinne an Reputation in dem du deine Ideen verwirklichst",
         EN: "5. Win awesome prices and magnify your reputation by sharing your ideas."
     }, faq3_p6: {
-        DE: "...",
+        DE: "6. Du wirst neue Leute kennen lernen und eine Menge Spaß haben!",
         EN: "6. Meet new people & have lots of fun!"
     }, faq4_head: {
-        DE: "...",
+        DE: "Für wen wurde die Veranstaltung konzipiert?",
         EN: "Who is EduHacks for?"
     }, faq4_intro: {
-        DE: "...",
+        DE: "Jeder ist willkommen teilzunehmen. Wir streben einen bunten Teilnehmer Mix an, allerdings sind folgende Rollen typischer Weise auf einem Hackathon vertreten:",
         EN: "Everyone is welcome to join the event. We strive for an interdisciplinary mixture of participants, where everybody can contribute their specific skills. However, we recommend having a background in one of the following areas:"
     }, faq4_p1: {
-        DE: "...",
+        DE: "Programmierer",
         EN: "Programming"
     }, faq4_p2: {
-        DE: "...",
+        DE: "Web Designer",
         EN: "Web Design"
     }, faq4_p3: {
-        DE: "...",
+        DE: "Daten Analysten",
         EN: "Statistics & Data Analysis"
     }, faq4_p4: {
-        DE: "...",
+        DE: "Kommunikations Designer",
         EN: "Communication Design"
     }, faq4_p4: {
-        DE: "...",
+        DE: "Lehrer",
         EN: "Educating or teaching"
     }, sponsoring_intro: {
-        DE: "...",
+        DE: "Die ideale Veranstaltung, um mit hoch motivierten Studenten in Kontakt zu kommen, um so zukünftige Angestellte zu finden",
         EN: "The ideal way to get in contact with highly motivated students and find future employees"
     }, sponsoring_p1: {
-        DE: "...",
+        DE: "Es sind unterschiedliche Sponsorenpakte verfügbar",
         EN: "Different sponsoring packages are available"
     }, sponsoring_p2: {
-        DE: "...",
+        DE: "Teilnehmer von unterschiedlichen Studienbereichen: (Wirtschafts) Informatik, BWL, Design und andere",
         EN: "Participants from different fields of study: Business, IT, Design and others..."
     }, sponsoring_p3: {
-        DE: "...",
+        DE: "Kontaktieren Sie uns für weitere Informationen",
         EN: "Contact us for further information"
     }, sponsoring_p4: {
-        DE: "...",
+        DE: "<a class='btn btn btn-lg' target='_blank' href='docs/sponsorenpakete-2015-v2-EN.pdf' role='button'>Sponsorenpakete(EN)</a><a class='btn btn btn-lg' target='_blank' href='docs/sponsorenpakete-2015-v2-DE.pdf' role='button'>Sponsorenpakete (DE)</a>",
         EN: "<a class='btn btn btn-lg' target='_blank' href='docs/sponsorenpakete-2015-v2-EN.pdf' role='button'>Available Sponsoring Packages (EN)</a><a class='btn btn btn-lg' target='_blank' href='docs/sponsorenpakete-2015-v2-DE.pdf' role='button'>Available Sponsoring Packages (DE)</a>"
     }, sponsoring_p5: {
-        DE: "...",
+        DE: "<a class='btn btn-primary btn-lg' href='mailto:sponsoring@hackerstolz.de?subject=Sponsoring EDUHacks' role='button'>Sende uns eine E-mail</a>",
         EN: "<a class='btn btn-primary btn-lg' href='mailto:sponsoring@hackerstolz.de?subject=Sponsoring EDUHacks' role='button'>Send us an email!</a>"
     }
 };
